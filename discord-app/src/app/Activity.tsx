7f6b4e53582557caf918526c44280f640f88461d
malawad <50057882+malawadd@users.noreply.github.com>
2024-10-18 01:39:17 +0300

    💀 openboard dgmjs 💀

--- discord-app/src/app/Activity.tsx
+++ discord-app/src/app/Activity.tsx
@@ -1,9 +1,45 @@
 import { useEffect, useState } from 'react'
 import { useDiscordSdk } from '../hooks/useDiscordSdk'
+import { Editor, FillStyle, Shape, Text, type ShapeProps } from '@dgmjs/core';
+import { DGMEditor } from '@dgmjs/react';
+import data from './data.json';
+import { Palette } from './palette';
 
+declare global {
+	interface Window {
+	  editor: Editor;
+	}
+  }
 export const Activity = () => {
 	const { authenticated, discordSdk, status } = useDiscordSdk()
 	const [channelName, setChannelName] = useState<string>()
+	const [editor, setEditor] = useState<Editor | null>(null);
+	const [activeHandler, setActiveHandler] = useState<string>('Select');
+  
+	const handleMount = async (editor: Editor) => {
+	  window.editor = editor;
+	  setEditor(editor);
+	  editor.loadFromJSON(data);
+	  editor.fitToScreen();
+	  window.addEventListener('resize', () => {
+		editor.fit();
+	  });
+	};
+  
+	const handleShapeInitialize = (shape: Shape) => {
+	  shape.fillStyle =
+		shape instanceof Text ? FillStyle.NONE : FillStyle.HACHURE;
+	  shape.fillColor = '$green6';
+	  shape.fontFamily = 'Gloria Hallelujah';
+	  shape.fontSize = 20;
+	  shape.roughness = 1;
+	};
+  
+	const handlePropsChange = (props: ShapeProps) => {
+	  window.editor.actions.update(props);
+	};
+	
+  
 
 	useEffect(() => {
 		// Requesting the channel in GDMs (when the guild ID is null) requires
@@ -22,13 +58,22 @@ export const Activity = () => {
 	}, [authenticated, discordSdk])
 
 	return (
+		<>
 		<div className="m-0 flex min-h-screen min-w-80 flex-col place-items-center justify-center">
 			<img src="/rocket.png" className="my-4 h-24 duration-300 hover:drop-shadow-[0_0_2em_#646cff]" alt="Discord" />
 			<h1 className="my-4 text-5xl font-bold">Hello, World</h1>
 			<h3 className="my-4 font-bold">{channelName ? `#${channelName}` : status}</h3>
-			<small className="my-4">
-				Powered by <strong>Robo.js</strong>
-			</small>
+			
 		</div>
+		
+		<DGMEditor
+        className="absolute inset-0 border rounded-lg"
+        onMount={handleMount}
+        onShapeInitialize={handleShapeInitialize}
+        onActiveHandlerChange={(handler) => setActiveHandler(handler)}
+      />
+
+<Palette onPropsChange={handlePropsChange} />
+	  </>
 	)
 }
--- discord-app/src/app/App.tsx
+++ discord-app/src/app/App.tsx
@@ -2,6 +2,7 @@ import { DiscordContextProvider } from '../hooks/useDiscordSdk'
 import { Activity } from './Activity'
 import './App.css'
 
+
 /**
  * Set `authenticate` to true to enable Discord authentication.
  * You can also set the `scope` prop to request additional permissions.
